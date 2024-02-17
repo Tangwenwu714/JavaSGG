a47d8da1b6990830774adf0a3db178e770c7ef75
Liu, Steve Yuliu <60839598+Tangwenwu714@users.noreply.github.com>
2024-02-17 21:08:41 +0800

    Add files via upload

---
+++ oop2/override/exer1/ManKind.java
@@ -0,0 +1,57 @@
+package com.atguigu04.override.exer1;
+
+/**
+ * ClassName: ManKind
+ * Package: com.atguigu03._extends.exer1
+ * Description:
+ *
+ * @Author Yubin-Liu
+ * @Create 2024/2/17 17:05
+ * @Version 1.0
+ */
+public class ManKind {
+    private int sex;
+    private int salary;
+    public ManKind() {
+    }
+
+    public ManKind(int sex, int salary) {
+        this.sex = sex;
+        this.salary = salary;
+    }
+
+
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void manOrWomen(){
+        if (sex == 1){
+            System.out.println("Man");
+        } else if (sex == 0) {
+            System.out.println("Woman");
+        }
+    }
+
+    public void employeed(){
+         if (salary == 0){
+             System.out.println("no job");
+         } else {
+             System.out.println("job");
+         }
+    }
+
+}
---
+++ oop2/override/exer2/Circle.java
@@ -0,0 +1,29 @@
+package com.atguigu04.override.exer2;
+
+/**
+ * ClassName: Circle
+ * Package: com.atguigu03._extends.exer2
+ * Description:
+ *
+ * @Author Yubin-Liu
+ * @Create 2024/2/17 17:41
+ * @Version 1.0
+ */
+public class Circle {
+    private double radius;
+    public Circle(){
+        this.radius = 1;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double findArea(){
+        return 3.14 * radius * radius;
+    }
+}
